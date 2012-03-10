b51a1f201c4de1fd8738f491c15046b1cc462aa8
Jim Schneider <JimboSchneider@gmail.om>
2012-03-10 16:43:32 -0700

    Trying to get git to work.

---
+++ Euler/Specs/Problem-3/When_finding_the_final_sum_Problem3.cs
@@ -0,0 +1,21 @@
+﻿using EulerLibrary;
+using Machine.Specifications;
+
+namespace ChangeMe
+{
+    [Subject(typeof(Problem3))]
+    public class When_finding_the_final_sum_Problem3
+    {
+        Establish context = () =>
+                                {
+                                    problem3 = new Problem3(600851475143);
+                                };
+
+        Because of = () => {};
+
+        It Should_not_be_null = () => problem3.ShouldNotBeNull();
+        It Should_find_LargestPrime = () => problem3.LargestPrime.ShouldEqual(1);
+
+        static Problem3 problem3;
+    }
+}
